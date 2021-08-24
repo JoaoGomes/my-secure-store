e41e2c0b723ddd916419981563d110a2d3ec4de6
João Eduardo Gomes <JoaoGomes@github.com>
2021-08-24 08:45:21 -0300

    Inicialização do repositório com mudanças no App().

--- App.js
+++ App.js
@@ -1,21 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button }  from 'react-native';
+import * as SecureStore from 'expo-secure-store';
 
 export default function App() {
+	const [data, setData] = useState();
+	async function onSaveSecure()  {
+		await SecureStore.setItemAsync('token', 'my-token-value');
+  }
+
+  async function onReadSecure() {
+	  const myToken = await SecureStore.getItemAsync('token');
+	  console.log(myToken);
+	  setDAta(myToken);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+	  <View style={styles.container}>
+		  <Text>Secure Store</Text>
+		  <Button onPress={onSaveSecure} title="Salvar" color="#841584" />
+		  <Button onPress={onReadSecure} title="Ler" color="#45AA89" />
+		  { data && <Text>{data}</Text> }
+	  </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
 });
